e5a8f040755749c9ef6731dd5e87436c0ebe681b
Akhil Krishnakumar <akhil.krishnakumar@springernature.com>
2020-11-26 21:38:57 +0530

    Complete InstructionProcessor requirement - should turn off all ACs on adjacent sub corridors if consumption goes out of budget

--- src/test/kotlin/InstructionProcessorTest.kt
+++ src/test/kotlin/InstructionProcessorTest.kt
@@ -8,6 +8,7 @@ class InstructionProcessorTest {
 
     @Test
     internal fun `should turn on lights in sub corridor when motion is detected`() {
+        // TODO: Need to change test data where budgeting is not applied
         val expectedStatus = sampleUpdatedStatus
         val initialStatus = defaultStatus()
         val inputSignal = MotionDetected(1, 2, initialStatus)
@@ -33,7 +34,7 @@ class InstructionProcessorTest {
             Floor(
                 1,
                 listOf(MainCorridor(1, true, true)),
-                listOf(SubCorridor(1, false, true), SubCorridor(2, true, true))
+                listOf(SubCorridor(1, false, false), SubCorridor(2, true, true))
             ),
             Floor(
                 2,
