92001ca84b36e8aa5de3832dd717c194816c0e30
Akhil Krishnakumar <akhil.krishnakumar@springernature.com>
2020-11-27 09:11:18 +0530

    Create Failing Business Requirement Test - Juice Master will turn off lights in sub corridor and turn on all ACs when there is no motion detected for a minute

--- src/test/kotlin/ConsoleAppTest.kt
+++ src/test/kotlin/ConsoleAppTest.kt
@@ -1,8 +1,11 @@
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
+import java.lang.Thread.sleep
 
 class ConsoleAppTest {
+
+    private val noMotionTimeout: Long = 100
 
     private val consoleQueue = ArrayDeque<String>()
     private fun consoleOutputStream(output: String) = consoleQueue.addLast(output)
@@ -35,6 +38,22 @@ class ConsoleAppTest {
         assertThat(actualStatus, equalTo(expectedStatus))
     }
 
+    @Test
+    internal fun `Juice Master will turn off lights in sub corridor and turn on all ACs when there is no motion detected for a minute`() {
+        val expectedStatus = statusAfterNoMotionForAMin
+
+        consoleApp.consume(sampleMotionSignal)
+        consoleApp.status()
+        simulateWaitingForAMinute()
+        consoleApp.status()
+
+        val actualStatus = consoleQueue[1]
+
+        assertThat(actualStatus, equalTo(expectedStatus))
+    }
+
+    private fun simulateWaitingForAMinute() = sleep(noMotionTimeout)
+
     val defaultStatus = """
         Floor 1
         Main corridor 1 Light 1 : ON AC : ON
@@ -51,6 +70,17 @@ class ConsoleAppTest {
         Main corridor 1 Light 1 : ON AC : ON
         Sub corridor 1 Light 1 : OFF AC : OFF
         Sub corridor 2 Light 2 : ON AC : ON
+        Floor 2
+        Main corridor 1 Light 1 : ON AC : ON
+        Sub corridor 1 Light 1 : OFF AC : ON
+        Sub corridor 2 Light 2 : OFF AC : ON
+    """.trimIndent()
+
+    val statusAfterNoMotionForAMin = """
+        Floor 1
+        Main corridor 1 Light 1 : ON AC : ON
+        Sub corridor 1 Light 1 : OFF AC : ON
+        Sub corridor 2 Light 2 : OFF AC : ON
         Floor 2
         Main corridor 1 Light 1 : ON AC : ON
         Sub corridor 1 Light 1 : OFF AC : ON
